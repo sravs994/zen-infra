b67aabb75079664f18662d4e4e2bd765becd14c6
sravs994 <yash061994@gmail.com>
2026-05-15 17:02:40 +0530

    config: update bucket name and github org for my account

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-dpp-2026"  # Replace with your S3 bucket name
+    bucket       = "zen-pharma-terraform-state-srav1994"  # Replace with your S3 bucket name
     key          = "envs/prod/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
--- envs/qa/variables.tf
+++ envs/qa/variables.tf
@@ -13,5 +13,5 @@ variable "jwt_secret" {
 variable "github_org" {
   description = "GitHub username or organization that owns zen-pharma-frontend and zen-pharma-backend (e.g. john-smith)"
   type        = string
-  default     = "DPP-2026"
+  default     = "sravs994"
 }
